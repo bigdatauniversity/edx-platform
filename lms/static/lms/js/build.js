e155348d8566f0cab0acb30622fce8df2c6dad84
Michael Lin <linzichunzf@hotmail.com>
2017-06-07 15:17:43 -0400

    add coursware_factory

--- lms/static/lms/js/build.js
+++ lms/static/lms/js/build.js
@@ -18,6 +18,7 @@
          * done.
          */
         modules: getModulesList([
+            'js/courseware/courseware_factory',
             'js/discovery/discovery_factory',
             'js/edxnotes/views/notes_visibility_factory',
             'js/edxnotes/views/page_factory',
